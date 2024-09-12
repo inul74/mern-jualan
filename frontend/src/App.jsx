3bb9291ae4be296a9fd08642ad9a12ecc617c925
inul74 <zmusthofa74@gmail.com>
2024-09-12 23:20:37 +0700

    create auction item

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,6 +15,7 @@ import About from "./pages/About";
 import Leaderboard from "./pages/Leaderboard";
 import Auctions from "./pages/Auctions";
 import AuctionItem from "./pages/AuctionItem";
+import CreateAuction from "./pages/CreateAuction";
 
 const App = () => {
   const dispatch = useDispatch();
@@ -37,6 +38,7 @@ const App = () => {
         <Route path="/leaderboard" element={<Leaderboard />} />
         <Route path="/auctions" element={<Auctions />} />
         <Route path="/auction/item/:id" element={<AuctionItem />} />
+        <Route path="/create-auction" element={<CreateAuction />} />
       </Routes>
       <ToastContainer position="top-right" />
     </Router>
